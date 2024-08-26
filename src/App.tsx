2a461cc383e17e4c6cc666e61ad4314ff5431d0a
chaoting <chaoting666@gmail.com>
2024-08-26 13:43:05 +0800

    feat: add GA4

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,14 @@ import routes from "routes";
 import PageLoading from "pages/PageLoading";
 import TimersProvider from 'context/Timers/Timers.provider';
 // import OfflineHandle from "components/OfflineHandle";
+import ServiceGA4 from "services/ga4.service";
 
 function App() {
+  ServiceGA4.init();
+  window.onpageshow = function (event) {
+    if (event.persisted) window.location.reload();
+  };
+
   const element = useRoutes(routes);
 
   return (<>
