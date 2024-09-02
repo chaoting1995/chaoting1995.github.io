3129d8b4f0a82d14d227f9c3820aeac6fe92e25e
chaoting <chaoting666@gmail.com>
2024-09-03 02:26:13 +0800

    fix: 調整：音頻物件，複用＋結束時銷毀

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -6,37 +6,42 @@ import rollingAudio from 'audio/rolling.mp3';
 import rolling2Audio from 'audio/rolling2.mp3';
 import celebrationAudio from 'audio/celebration.mp3';
 
-const soundClick = new Howl({ src: [clickAudio], html5: true, pool: 10 });
-const soundBell = new Howl({ src: [bellAudio], html5: true, pool: 10 });
-const soundRolling = new Howl({ src: [rollingAudio], html5: true, pool: 10 });
-const soundRolling2 = new Howl({ src: [rolling2Audio], html5: true, pool: 10 });
-const soundCelebration = new Howl({ src: [celebrationAudio], html5: true, pool: 10 });
+let soundClick: Howl | null = null;
+let soundBell: Howl | null = null;
+let soundRolling: Howl | null = null;
+let soundRolling2: Howl | null = null;
+let soundCelebration: Howl | null = null;
 
 function audioClick() {
+  if (!soundClick) soundClick = new Howl({ src: [clickAudio], html5: true, pool: 10 });
   soundClick.play();
 }
 
 function audioBell() {
+  if (!soundBell) soundBell = new Howl({ src: [bellAudio], html5: true, pool: 10 });
   soundBell.play();
 }
 
 function audioRolling() {
+  if (!soundRolling) soundRolling = new Howl({ src: [rollingAudio], html5: true, pool: 10 });
   soundRolling.play();
 }
 
 function audioRolling2() {
+  if (!soundRolling2) soundRolling2 = new Howl({ src: [rolling2Audio], html5: true, pool: 10 });
   soundRolling2.play();
 }
 
 function audioCelebration() {
+  if (!soundCelebration) soundCelebration = new Howl({ src: [celebrationAudio], html5: true, pool: 10 });
   soundCelebration.play();
 }
 function cleanupAudio() {
-  soundClick.unload();
-  soundBell.unload();
-  soundRolling.unload();
-  soundRolling2.unload();
-  soundCelebration.unload();
+  soundClick?.unload();
+  soundBell?.unload();
+  soundRolling?.unload();
+  soundRolling2?.unload();
+  soundCelebration?.unload();
 }
 
 const UtilAudio = {
