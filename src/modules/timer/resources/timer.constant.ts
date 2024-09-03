9158f9f32f79a5cc29fa1dec4c91d8e652ecb3b1
chaoting <chaoting666@gmail.com>
2024-09-03 12:53:33 +0800

    feat: timer.constant.ts > 新增預設值與模板＋更改順序

--- src/modules/timer/resources/timer.constant.ts
+++ src/modules/timer/resources/timer.constant.ts
@@ -7,17 +7,29 @@ export const DEFAULT_TIMERS: Array<Timer> = [
   {
     id: 'debate-timer-001',
     mode: EnumTimerMode.Normal,
-    name: '新式奧瑞岡333制',
-    ring: [150, 180, 210]
-  },
-  {
-    id: 'debate-timer-002',
-    mode: EnumTimerMode.Normal,
     name: '新式奧瑞岡444制',
     ring: [210, 240, 270]
   },
   {
+    id: 'debate-timer-002',
+    mode: EnumTimerMode.Normal,
+    name: '新式奧瑞岡333制',
+    ring: [150, 180, 210]
+  },
+  {
     id: 'debate-timer-003',
+    mode: EnumTimerMode.Normal,
+    name: '新式奧瑞岡554制',
+    ring: [240, 270, 300]
+  },
+  {
+    id: 'debate-timer-004',
+    mode: EnumTimerMode.Normal,
+    name: '新式奧瑞岡554制-結辯',
+    ring: [180, 210, 240]
+  },
+  {
+    id: 'debate-timer-005',
     mode: EnumTimerMode.Crossfire,
     name: '自由辯論',
     ring: [210, 240]
@@ -28,14 +40,26 @@ export const TEMPLATE_TIMERS: Array<Timer> = [
   {
     id: '',
     mode: EnumTimerMode.Normal,
+    name: '新式奧瑞岡444制',
+    ring: [210, 240, 270]
+  },
+  {
+    id: '',
+    mode: EnumTimerMode.Normal,
     name: '新式奧瑞岡333制',
     ring: [150, 180, 210]
   },
   {
     id: '',
     mode: EnumTimerMode.Normal,
-    name: '新式奧瑞岡444制',
-    ring: [210, 240, 270]
+    name: '新式奧瑞岡554制',
+    ring: [240, 270, 300]
+  },
+  {
+    id: '',
+    mode: EnumTimerMode.Normal,
+    name: '新式奧瑞岡554制-結辯',
+    ring: [180, 210, 240]
   },
   {
     id: '',
--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -6,6 +6,18 @@ import rollingAudio from 'audio/rolling.mp3';
 // import rolling2Audio from 'audio/rolling2.mp3';
 import celebrationAudio from 'audio/celebration.mp3';
 
+// 兩難：
+// 網頁載入時，先不初始化 Howl，則手機版第一次音效會延遲
+// let soundClick: Howl | null = null;
+// let soundBell: Howl | null = null;
+// let soundRolling: Howl | null = null;
+// let soundRolling2: Howl | null = null;
+// let soundCelebration: Howl | null = null;
+
+// 網頁載入時，立刻初始化 Howl，則出現以下警告資訊
+// The AudioContext was not allowed to start. It must be resumed (or created) after a user gesture on the page.
+//  HTML5 Audio pool exhausted, returning potentially locked audio object.
+
 const soundClick = new Howl({ src: [clickAudio], html5: true, pool: 50 });
 const soundBell = new Howl({ src: [bellAudio], html5: true, pool: 50 });
 const soundRolling = new Howl({ src: [rollingAudio], html5: true, pool: 50 });
