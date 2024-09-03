20b178c4a538414b50fe206067597e0d6b2dc066
chaoting <chaoting666@gmail.com>
2024-09-03 11:16:17 +0800

    refactor: 重構音頻的初始化時機

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -6,54 +6,47 @@ import rollingAudio from 'audio/rolling.mp3';
 import rolling2Audio from 'audio/rolling2.mp3';
 import celebrationAudio from 'audio/celebration.mp3';
 
-let soundClick: Howl | null = null;
-let soundBell: Howl | null = null;
-let soundRolling: Howl | null = null;
-let soundRolling2: Howl | null = null;
-let soundCelebration: Howl | null = null;
+const soundClick = new Howl({ src: [clickAudio], html5: true, pool: 50 });
+const soundBell = new Howl({ src: [bellAudio], html5: true, pool: 50 });
+const soundRolling = new Howl({ src: [rollingAudio], html5: true, pool: 50 });
+// const soundRolling2 = new Howl({ src: [rolling2Audio], html5: true, pool: 50 });
+const soundCelebration = new Howl({ src: [celebrationAudio], html5: true, pool: 50 });
 
 function audioClick() {
-  if (!soundClick) soundClick = new Howl({ src: [clickAudio], html5: true, pool: 10 });
   soundClick.play();
 }
 
 function audioBell() {
-  if (!soundBell) soundBell = new Howl({ src: [bellAudio], html5: true, pool: 10 });
   soundBell.play();
 }
 
 function audioRolling() {
-  if (!soundRolling) soundRolling = new Howl({ src: [rollingAudio], html5: true, pool: 10 });
   soundRolling.play();
 }
 
-function audioRolling2() {
-  if (!soundRolling2) soundRolling2 = new Howl({ src: [rolling2Audio], html5: true, pool: 10 });
-  soundRolling2.play();
-}
+// function audioRolling2() {
+//   soundRolling2.play();
+// }
 
 function audioCelebration() {
-  if (!soundCelebration) soundCelebration = new Howl({ src: [celebrationAudio], html5: true, pool: 10 });
   soundCelebration.play();
 }
+
 function cleanupAudio() {
-  soundClick?.unload();
-  soundBell?.unload();
-  soundRolling?.unload();
-  soundRolling2?.unload();
-  soundCelebration?.unload();
+  soundClick.unload();
+  soundBell.unload();
+  soundRolling.unload();
+  // soundRolling2.unload();
+  soundCelebration.unload();
 }
 
 const UtilAudio = {
   audioClick,
   audioBell,
   audioRolling,
-  audioRolling2,
+  // audioRolling2,
   audioCelebration,
   cleanupAudio
 };
 
 export default UtilAudio;
-
-// https://juejin.cn/post/7270831230077681722
-// 應複用音頻物件
