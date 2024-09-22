623571b05d2d8d0bbbe441aedd3f77470cbe070a
chaoting <chaoting666@gmail.com>
2024-09-23 01:37:09 +0800

    fix: EnumArgumentStatus

--- src/modules/listening/enums/enumArgumentStatus.ts
+++ src/modules/listening/enums/enumArgumentStatus.ts
@@ -3,9 +3,9 @@ export enum EnumArgumentStatus {
   HP100 = 'HP100',                             // 完全成立
   HP90 = 'HP90',                               // 削弱
   HP10 = 'HP10',                               // 極大削弱
-  HP0_Obfuscation = 'HP0_Obfuscation',         // 擊倒(打糊)
-  HP0_InitFail = 'HP0_InitFail',               // 初步不成立
-  HP0_Disassembly = 'HP0_Disassembly',         // 擊倒(拆掉)
+  HP0Obfuscation = 'HP0Obfuscation',         // 擊倒(打糊)
+  HP0InitFail = 'HP0InitFail',               // 初步不成立
+  HP0Disassembly = 'HP0Disassembly',         // 擊倒(拆掉)
   Lose = 'Lose',                               // 掉點
   Unknown = 'Unknown',                         // 未知
   Other = 'Other',                             // 其他
