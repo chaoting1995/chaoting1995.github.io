afe54457477ee0951ca9ad184a0b100ccec2a6fe
chaoting <chaoting666@gmail.com>
2024-09-23 01:41:19 +0800

    fix: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import eslint from 'vite-plugin-eslint';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 import packageFile from './package.json';
+import { ssr } from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +15,8 @@ export default defineConfig({
     react(),
     viteTsconfigPaths(),
     eslint({ include: 'src' }),
-    svgr()
+    svgr(),
+    ssr({ prerender: true })
   ],
   server: {
     port: 3000,
